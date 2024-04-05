d8a7e0e81de607978226b69ffccef1f603fc80ad
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-05 20:44:10 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Security.Cryptography;
+Console.WriteLine("--- Rolagem de Dado ---\n");
+
+Console.WriteLine("Quantidade de Faces: 20");
+Console.Write("Rolagem: ");
+Console.Write(RandomNumberGenerator.GetInt32(1, 21));
